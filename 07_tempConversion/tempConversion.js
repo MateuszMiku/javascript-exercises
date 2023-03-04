4e9e86532f415097cc510335a22e3f951e26d51a
MateuszMiku <mateuszmiku.mm@gmail.com>
2023-03-04 19:21:29 +0100

    finish temp task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    let celsiusResult = ((fahrenheit - 32) * 5 / 9);
+    let roundedC = Math.round(celsiusResult * 10) / 10;
+    return roundedC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheitResult = (celsius * 9 / 5 + 32);
+  let roundedF = Math.round(fahrenheitResult * 10) / 10;
+  return roundedF;
 };
 
 // Do not edit below this line
