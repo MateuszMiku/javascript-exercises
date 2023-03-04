ddc96e69ad510ac43ae27deef77d94c257a730bc
MateuszMiku <mateuszmiku.mm@gmail.com>
2023-03-04 18:18:39 +0100

    some exercises!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
-};
+const sumAll = function(x, y) {
+	if (x < 0 || y < 0 || !Number.isInteger(x)
+	 || !Number.isInteger(y)) {
+	return "ERROR";
+		} else {
+			if (x < y) {
+				let summed = 0;
+					for (i = x; i<=y; i++) {
+						summed += i;
+					}
+				return summed;
+			} else if (x > y) {
+				let summed = 0;
+					for (i = y; i<=x; i++) {
+						summed += i;
+					}
+				return summed;
+			}
+}};
 
 // Do not edit below this line
 module.exports = sumAll;
+
